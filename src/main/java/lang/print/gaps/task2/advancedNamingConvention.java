4172cc66249f4e8f42ac8e28110e9883122c0191
kokibas <nurym0807@gmail.com>
2022-10-18 15:08:59 +0600

    Module3 TAsk2

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -1,17 +1,17 @@
 package lang.print.gaps.task2;
 
 public class advancedNamingConvention {
-    private static final int adult_age = 18;
+    private static final int ADULT_AGE = 18;
 
     private int AGE;
-    private int phoneNumber;
+    private int number;
 
-    public static  void CAllToFriend() {
+     void callToFriend() {
         //some code
 
     }
 
-    public static void callByNumber(int Number) {
+     void callByNumber(int number) {
 
         //some code
     }
